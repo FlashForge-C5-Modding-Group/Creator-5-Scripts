b1ea62fdfe5c386943428a1b341d3c644e5ae1d6
Cart <81428538+Cartrigger@users.noreply.github.com>
2026-08-20 19:04:04 -0400

    Update enable-msmr.sh

--- scripts/scripts/enable-msmr.sh
+++ scripts/scripts/enable-msmr.sh
@@ -9,7 +9,7 @@
     done
 
     if pgrep -f "klippy" > /dev/null 2>&1; then
-        /usr/prog/nginx/sbin/nginx -p /usr/prog/nginx -c /usr/prog/nginx/conf/nginx.conf
+        /usr/prog/nginx/sbin/nginx -p /usr/prog/nginx -c /usr/prog/nginx/conf/nginx.conf &
         /usr/prog/klipper/moonrakerDaemon start
     fi
 ) &
